127e893ce35c31b6466bc1e3028f3133c47afb07
HoaFlee <lehoanghoa1008@gmail.com>
2024-05-23 16:45:12 +0700

    init code

---
+++ src/layouts/main/header.js
@@ -0,0 +1,112 @@
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import { useTheme } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Badge, { badgeClasses } from '@mui/material/Badge';
+
+import { paths } from 'src/routes/paths';
+
+import { useOffSetTop } from 'src/hooks/use-off-set-top';
+import { useResponsive } from 'src/hooks/use-responsive';
+
+import { bgBlur } from 'src/theme/css';
+
+import Logo from 'src/components/logo';
+import Label from 'src/components/label';
+
+import NavMobile from './nav/mobile';
+import NavDesktop from './nav/desktop';
+import { HEADER } from '../config-layout';
+import { navConfig } from './config-navigation';
+import LoginButton from '../common/login-button';
+import HeaderShadow from '../common/header-shadow';
+import SettingsButton from '../common/settings-button';
+
+// ----------------------------------------------------------------------
+export default function Header() {
+  const theme = useTheme();
+
+  const mdUp = useResponsive('up', 'md');
+
+  const offsetTop = useOffSetTop(HEADER.H_DESKTOP);
+
+  return (
+    <AppBar>
+      <Toolbar
+        disableGutters
+        sx={{
+          height: {
+            xs: HEADER.H_MOBILE,
+            md: HEADER.H_DESKTOP,
+          },
+          transition: theme.transitions.create(['height'], {
+            easing: theme.transitions.easing.easeInOut,
+            duration: theme.transitions.duration.shorter,
+          }),
+          ...(offsetTop && {
+            ...bgBlur({
+              color: theme.palette.background.default,
+            }),
+            height: {
+              md: HEADER.H_DESKTOP_OFFSET,
+            },
+          }),
+        }}
+      >
+        <Container sx={{ height: 1, display: 'flex', alignItems: 'center' }}>
+          <Logo />
+          <Badge
+            sx={{
+              [`& .${badgeClasses.badge}`]: {
+                top: -10,
+                right: -20,
+              },
+            }}
+            badgeContent={
+              <Link
+                href={paths.changelog}
+                target="_blank"
+                rel="noopener"
+                underline="none"
+                sx={{ ml: 1 }}
+              >
+                <Label color="info" sx={{ textTransform: 'unset', height: 22, px: 1 }}>
+                  Beta
+                </Label>
+              </Link>
+            }
+          />
+          {/* </Badge> */}
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          {mdUp && <NavDesktop data={navConfig} />}
+
+          <Stack alignItems="center" direction={{ xs: 'row', md: 'row-reverse' }}>
+            {/* <Button component={RouterLink} variant="contained" rel="noopener" href={paths.auth.jwt.register}>
+              Register
+            </Button> */}
+
+            {/* {mdUp && } */}
+            <LoginButton />
+
+            {/* <SettingsButton
+              sx={{
+                ml: { xs: 1, md: 0 },
+                mr: { md: 2 },
+              }}
+            /> */}
+
+            {!mdUp && <NavMobile data={navConfig} />}
+          </Stack>
+        </Container>
+      </Toolbar>
+
+      {offsetTop && <HeaderShadow />}
+    </AppBar>
+  );
+}
